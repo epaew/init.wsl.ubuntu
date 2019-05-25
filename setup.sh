1f7586ab9593a357153f338270bb8cca277e26b6
epaew <epaew.333@gmail.com>
2019-05-25 14:51:34 +0900

    add role and set umask

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -ux
 
+umask 022
 if ! which ansible-playbook >/dev/null; then
   sudo apt install -y python3-pip
   pip3 install --user --upgrade ansible
 fi
 
 ansible-playbook -i ansible/hosts \
-  --vault-password-file ansible/.vault_pass ansible/playbook.yml \
+  --vault-password-file ansible/.vault_pass ansible/playbook.yaml \
   --ask-become-pass
