2149f1ab1ff498b9ff7b9ae4f9df8a20edd2ce2b
epaew <epaew.333@gmail.com>
2019-05-25 19:14:55 +0900

    add neovim role

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 umask 022
 if ! which ansible-playbook >/dev/null; then
   sudo apt install -y python3-pip
-  pip3 install --user --upgrade ansible
+  pip3 install --user --upgrade ansible setuptools virtualenv
 fi
 
-ansible-playbook -i ansible/hosts \
-  --vault-password-file ansible/.vault_pass ansible/playbook.yaml \
-  --ask-become-pass
+ansible-playbook --ask-become-pass \
+  --inventory ansible/hosts \
+  --vault-password-file ansible/.vault_pass \
+  ansible/playbook.yaml
