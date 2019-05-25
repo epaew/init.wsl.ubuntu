ef8096c8b449c0c12bf99375f30d6a7cd71e8df3
epaew <epaew.333@gmail.com>
2019-05-25 12:57:14 +0900

    update playbook.yml and setup.sh

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ```
 git clone https://github.com/epaew/init.wsl.ubuntu.git
 cd init.wsl.ubuntu
+# put ansible-vault password file at ansible/.vault_pass
 ./setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-#!/bin/sh -eux
+#!/bin/sh -ux
 
-sudo apt install -y python3-pip
-pip3 install --user --upgrade ansible
+if ! which ansible-playbook >/dev/null; then
+  sudo apt install -y python3-pip
+  pip3 install --user --upgrade ansible
+fi
 
-ansible-playbook -i ansible/hosts --vault-password-file ansible/.vault_pass ansible/playbook.yml
+ansible-playbook -i ansible/hosts \
+  --vault-password-file ansible/.vault_pass ansible/playbook.yml \
+  --ask-become-pass
