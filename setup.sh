6a3432a7e37f6d2d8cc0b14df080b58028113c4c
epaew <epaew.333@gmail.com>
2020-05-17 23:19:58 +0900

    Update setup.sh to use pipenv

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -ux
 
 cd "${0%/*}" # Move to this script's directory.
-
 umask 022
-if ! which ansible-playbook >/dev/null; then
-  sudo apt install -y python3-pip
-  pip3 install --user --upgrade ansible setuptools virtualenv
-  source ~/.profile
+
+if ! (which pip3 >/dev/null) then
+  sudo apt install -y python3-pip pipenv
 fi
 
-ansible-playbook --ask-become-pass \
+source ~/.profile
+pipenv install
+pipenv run ansible-playbook --ask-become-pass \
   --inventory ansible/hosts \
   --vault-password-file ansible/.vault_pass \
   ansible/playbook.yaml
